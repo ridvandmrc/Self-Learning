643f23ea4dc9ae11c7755abc1c565ffa86893c41
ridvandmrc <ridvan.demirci@accenture.com>
2022-01-15 23:50:47 +0300

    added examples for grammer and types

---
+++ javascript/grammer_and_types/example.js
@@ -0,0 +1,25 @@
+{
+    let name = 'ridvan';
+    console.log(name)
+}
+
+// console.log(name) // it will throw exception
+
+// Type conversion
+
+var answer = 45;
+answer = '45';
+answer = true;
+
+// '+' operators
+
+console.log('answer is '+ 45)
+
+// convert string to numbers
+
+console.log(typeof '45') // return string
+console.log(typeof +'45') // return number
+console.log(parseInt('450')) // return  450
+console.log(parseInt('a4as50')) // return NaN
+console.log(parseFloat('4.5')) // return 4.5
+console.log(parseFloat('a4.5')) // return NaN
