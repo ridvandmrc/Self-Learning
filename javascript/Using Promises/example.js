99780c2bdd0ba0428827c464c2440787d355687f
ridvandmrc <ridvan.demirci@accenture.com>
2022-01-26 00:09:41 +0300

    completed promise example

--- javascript/Using Promises/example.js
+++ javascript/Using Promises/example.js
@@ -31,3 +31,48 @@ runCallback(null)
   .catch(error => {
     console.log(error);
   });
+
+/*  Resolve and reject   */
+const asyncLog = () => {
+  return new Promise(resolve => {
+    resolve;
+  });
+};
+
+asyncLog().then(console.log("resolve"));
+
+/* ----- Run async and race condition --------- */
+
+const waitUntil = (duration, cb) => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve("called");
+    }, duration);
+  });
+};
+
+Promise;
+
+const p1 = waitUntil(1000).then(resolve => {
+  console.log("wait 1000ms");
+});
+
+const p2 = waitUntil(100).then(resolve => {
+  console.log("wait 100ms");
+});
+
+const p3 = waitUntil(10).then(resolve => {
+  console.log("wait 10ms");
+});
+
+Promise.race([p1, p2, p3]).then(res => {
+  console.log("first came: ", res); // called when a promise is done
+});
+
+Promise.all([p1, p2, p3]).then(results => {
+  console.log("All Done: ", results); // this is called all promises are done
+});
+
+Promise.allSettled([p1, p2, p3]).then(results => {
+  console.log("All settle Done: ", results); // this is called all promises are done
+});
